b680a3ee18b4749f7e3605a415f8d496e9361296
wushiyuan <wushiyuanwork@outlook.com>
2020-05-19 15:23:54 +0800

    vue2

---
+++ vue-demo/src/components/hello.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <el-radio v-model="radio" label="1">备选项</el-radio>
+    <el-radio v-model="radio" label="2">备选项</el-radio>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App ~~',
+      radio: '2'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style></style>
